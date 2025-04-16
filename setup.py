a51da1862141f63c6350624c6a2dc51dbc156592
BaoNguyenz <baohuulenguyen@gmail.com>
2025-04-16 22:02:42 +0700

    folder setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import fine_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'NguyenNguyen',
+    author_email = 'baohuulenguyen@gmail.com',
+    packages = fine_packages(),
+    install_requires = []
+)
